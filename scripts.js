a2f40134cfbe4f425e9098a48873932d4e7b0647
TristanCS <savage.tristan@gmail.com>
2023-10-03 10:22:29 +0200

    Added form validation

--- scripts.js
+++ scripts.js
@@ -2,26 +2,29 @@ const howDidWeDo = document.getElementById("how-did-we-do");
 const thankYou = document.getElementById("thank-you");
 const submitButtons = document.querySelectorAll("input[type=submit]");
 const ratingButtons = document.querySelectorAll("button");
+let isSelected = false;
 
-selectRating();
-submitForm();
-
-function selectRating() {
+(function selectRating() {
   ratingButtons.forEach((ratingButton) => {
     ratingButton.addEventListener("click", (event) => {
       ratingButtons.forEach((btn) => {
         btn.classList.remove("selected");
       });
       ratingButton.classList.add("selected");
+      isSelected = true;
     });
   });
-}
+})();
 
-function submitForm() {
+(function submitForm() {
   submitButtons.forEach((submitButton) => {
     submitButton.addEventListener("click", (event) => {
-      howDidWeDo.classList.add("hidden");
-      thankYou.classList.remove("hidden");
+      if (!isSelected) {
+        alert("Please select a rating");
+      } else {
+        howDidWeDo.classList.add("hidden");
+        thankYou.classList.remove("hidden");
+      }
     });
   });
-}
+})();
